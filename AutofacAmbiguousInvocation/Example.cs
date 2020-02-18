96fdd70e5e4b615287676a681bf246fcc00cf68d
Vladimir Shkolka <ztBlackGad@gmail.com>
2020-02-18 15:28:22 +0200

    Add project files.

---
+++ AutofacAmbiguousInvocation/Example.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace AutofacAmbiguousInvocation
+{
+    public class Example
+    {
+        #region Constructors
+
+        public Example()
+        {
+            ILifetimeScope scope = null;
+            var instance = scope.ResolveOptional<IInterface>();
+        }
+
+        #endregion
+    }
+
+    internal interface IInterface
+    {
+    }
+}
